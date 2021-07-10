56955d37ad0bba2b83a4db6db2f8e2d4cbd48bba
Thawng Hmung <70231710+tthmung@users.noreply.github.com>
2021-07-10 12:34:45 -0400

    Add files via upload

---
+++ scholarship/README.txt
@@ -0,0 +1,8 @@
+The purpose of this program is to read in a title of a scholarship and it will match the title with titles from a file. 
+If the user want to add the title to the list of title, it will add the title to the file and can be used for future use.
+Each title will have a refernce to an essay file, the user can acess this essay file if they wish to after searching for a title.
+Searching for a scholarship can be very frustrating and sometime many of them are similar espically the one the require essays.
+This program will hopefully make it much easier. All the commands and file searching is done through the terminal. 
+
+
+GUI VERSION COMING SOON!!! (HOPEFULLY)
---
+++ scholarship/main.py
@@ -0,0 +1,51 @@
+import re
+
+words = []
+
+# Review the titles and put them into a array for future use
+def reviewInits():
+    reader = open("titles.txt", "r")
+    
+    if (reader is None):
+        return 0
+    
+    size = 0
+    i = 0
+    for element in reader:
+        s = element
+        words.append(s)
+        size = size + 1
+
+    reader.close()
+    return size
+
+# Convert a sentence into a array
+def convert(lst):
+    return ([i for item in lst for i in item.split()])
+
+# Review the matching word, from the bank of titles
+def reviewMatch(targetWords, targets):
+    arr =  []
+    arr.append(targetWords)
+
+    # Convert the sentence into array of individual words
+    lst = convert(arr)
+    total = len(lst)
+    match = 0
+
+    print("'", targetWords, "'", sep = "", end= "")
+
+    # Find the matching words
+    for tar in target:
+        for element in lst:
+            if element == tar:
+                match += 1
+    
+    print(", total " , total ,", matched ", match, ", percentage ", float(match / total), sep = "")
+
+reviewInits()
+target = ["What is life and death"]
+target = convert(target)
+
+for element in words:
+    reviewMatch(element.strip(), target)
